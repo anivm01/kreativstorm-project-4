2c1fded4a9a350096de06c0646796acb00814c79
anivm01 <anivm.webdev@gmail.com>
2023-08-08 16:11:03 -0400

    calculator is working, some validation missing, still need percentage and sign buttons

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
-const currentOperandElement = document.querySelector(".current")
-const previousOperandElement = document.querySelector(".previous")
+const inputElement = document.querySelector(".current")
+const fullOperationElement = document.querySelector(".previous")
 const numbers = document.querySelectorAll(".number")
 const operators = document.querySelectorAll(".operator")
 const clearButton = document.querySelector(".clear")
@@ -8,64 +8,121 @@ const percentage = document.querySelector(".percentage")
 const equalsButton = document.querySelector(".equals")
 
 
-let currentOperand = ""
-let previousOperand = ""
-let operation
+let input = ""
+let fullOperation = []
 
-const updateDisplay = () => {
-    currentOperandElement.innerText = currentOperand
-    previousOperandElement.innerText = previousOperand
+const clear = () => {
+    input = ""
+    fullOperation = []
+    return
+}
+
+const updateDisplay = (top, bottom) => {
+    inputElement.innerText = top
+    fullOperationElement.innerText = bottom.join(" ")
+    console.log(fullOperation)
+    return
 }
 
 const appendNumber = (number) => {
-    if (number === '.' && currentOperand.includes('.')) {
+    if (number === '.' && input.includes('.')) {
         return
     }
-    currentOperand = currentOperand.toString() + number.toString()
+    input = input.toString() + number.toString()
 }
 
-const chooseOperation = (operation) => {
-    if (currentOperand === '') return
-    if (previousOperand !== '') {
-        compute()
+const addInputToOperation = () => {
+    if (input === "") {
+        return
     }
-    operation = operation
-    previousOperand = currentOperand
-    currentOperand = ""
+    const newInput = parseFloat(input)
+    if (newInput === NaN) {
+        return
+    }
+    fullOperation.push(newInput)
+    return
 }
 
-const compute = (firstOperant, operator, secondOperant) => {
-    let computation
-    if (isNaN(firstOperant) || isNaN(secondOperant)) return
-    switch (operator) {
-        case '+':
-            computation = firstOperant + secondOperant
-            break
-        case '-':
-            computation = firstOperant - secondOperant
-            break
-        case 'x':
-            computation = firstOperant * secondOperant
-            break
-        case '÷':
-            computation = firstOperant / secondOperant
-            break
-        default:
-            return
+const chooseOperation = (operator) => {
+    addInputToOperation()
+    fullOperation.push(operator)
+    input = ""
+}
+
+
+const compute = (fullOperationArray) => {
+    if (fullOperationArray.length < 3) {
+        return
     }
-    return computation
+    for (let i = 0; i < fullOperationArray.length; i++) {
+        if (fullOperationArray.includes("x")) {
+            const mulitplyIndex = fullOperationArray.indexOf("x")
+            const result = fullOperationArray[mulitplyIndex - 1] * fullOperationArray[mulitplyIndex + 1]
+            fullOperationArray.splice((mulitplyIndex - 1), 3, result)
+        }
+        console.log(fullOperationArray)
+    }
+    for (let i = 0; i < fullOperationArray.length; i++) {
+        if (fullOperationArray.includes("÷")) {
+            const divideIndex = fullOperationArray.indexOf("÷")
+            const result = fullOperationArray[divideIndex - 1] / fullOperationArray[divideIndex + 1]
+            fullOperationArray.splice((divideIndex - 1), 3, result)
+        }
+        console.log(fullOperationArray)
+    }
+    for (let i = 0; i < fullOperationArray.length; i++) {
+        if (fullOperationArray.includes("+")) {
+            const addIndex = fullOperationArray.indexOf("+")
+            const result = fullOperationArray[addIndex - 1] + fullOperationArray[addIndex + 1]
+            fullOperationArray.splice((addIndex - 1), 3, result)
+        }
+        console.log(fullOperationArray)
+    }
+    for (let i = 0; i < fullOperationArray.length; i++) {
+        if (fullOperationArray.includes("-")) {
+            const minusIndex = fullOperationArray.indexOf("-")
+            const result = fullOperationArray[minusIndex - 1] - fullOperationArray[minusIndex + 1]
+            fullOperationArray.splice((minusIndex - 1), 3, result)
+        }
+        console.log(fullOperationArray)
+    }
+
+    return fullOperationArray[0]
+}
+
+const evaluate = () => {
+    addInputToOperation()
+    const fullOperationCopy = [...fullOperation]
+    result = compute(fullOperation)
+    const computationArray = [...fullOperationCopy, "=", result]
+    input = result
+    fullOperation = []
+    return {
+        result: result,
+        computation: computationArray
+    }
 }
 
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
         appendNumber(number.innerText)
-        updateDisplay()
+        updateDisplay(input, fullOperation)
     })
 })
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
         chooseOperation(operator.innerText)
-        updateDisplay()
+        updateDisplay(input, fullOperation)
     })
+})
+
+equalsButton.addEventListener("click", () => {
+    const final = evaluate()
+    updateDisplay(final.result, final.computation)
+})
+
+clearButton.addEventListener("click", () => {
+    clear()
+    updateDisplay(input, fullOperation)
 })
