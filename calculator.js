ad53558d4045bb96511c83a8f35045e9610968d4
anivm01 <anivm.webdev@gmail.com>
2023-08-11 23:00:30 -0400

    clean up console logs and commented out sections

--- calculator.js
+++ calculator.js
@@ -7,19 +7,13 @@ const toggleSign = document.querySelector(".toggle")
 const percentage = document.querySelector(".percentage")
 const equalsButton = document.querySelector(".equals")
 
-// let input = ""
 const fullOperation = {
     prev: "",
     current: "",
     operation: ""
 }
 
-let firstOperand = ""
-let secondOperand = ""
-let operation = ""
-
 const clear = () => {
-    // input = ""
     fullOperation.prev = ""
     fullOperation.current = ""
     fullOperation.operation = ""
@@ -36,10 +30,7 @@ const backspace = () => {
         fullOperation.current = ""
         return
     }
-    console.log(input)
     fullOperation.current = parseFloat(input)
-    console.log(fullOperation.current)
-
     return
 }
 
@@ -60,31 +51,10 @@ const appendNumber = (number) => {
     }
     input = input.toString() + number.toString()
     fullOperation.current = parseFloat(input)
-
-    console.log(fullOperation)
     return
 }
 
-// const addInputToOperation = () => {
-//     if (input === "") {
-//         return
-//     }
-//     const newInput = parseFloat(input)
-//     if (isNaN(newInput)) {
-//         return
-//     }
-//     // fullOperation.push(newInput)
-//     firstOperand = newInput
-//     console.log(firstOperand)
-//     console.log(secondOperand)
-//     console.log(operation)
-//     return
-// }
-
 const chooseOperation = (operator) => {
-    // if (input === "" && (fullOperation[fullOperation.length - 1] === "+" || fullOperation[fullOperation.length - 1] === "-" || fullOperation[fullOperation.length - 1] === "x" || fullOperation[fullOperation.length - 1] === "÷")) {
-    //     return
-    // }
     if (fullOperation.prev === "") {
         return
     }
@@ -93,7 +63,6 @@ const chooseOperation = (operator) => {
         fullOperation.prev = result
         fullOperation.operation = operator
         fullOperation.current = ""
-        console.log(fullOperation)
     }
     else if (fullOperation.current === "" && fullOperation.operation === "" && fullOperation.prev) {
         fullOperation.prev = fullOperation.prev
@@ -102,75 +71,10 @@ const chooseOperation = (operator) => {
         fullOperation.prev = fullOperation.current
         fullOperation.operation = operator
         fullOperation.current = ""
-        console.log(fullOperation)
     }
 
     return
 }
-
-// const compute = (fullOperationArray) => {
-//     if (input === "") {
-//         return
-//     }
-//     if (fullOperationArray.length < 3) {
-//         return
-//     }
-
-//     switch (operation) {
-//         case '+':
-//             computation = prev + current
-//             break
-//         case '-':
-//             computation = prev - current
-//             break
-//         case 'x':
-//             computation = prev * current
-//             break
-//         case '÷':
-//             computation = prev / current
-//             break
-//         default:
-//             return
-//     }
-
-
-
-
-//     for (let i = 0; i < fullOperationArray.length; i++) {
-//         if (fullOperationArray.includes("x")) {
-//             const mulitplyIndex = fullOperationArray.indexOf("x")
-//             const result = fullOperationArray[mulitplyIndex - 1] * fullOperationArray[mulitplyIndex + 1]
-//             fullOperationArray.splice((mulitplyIndex - 1), 3, result)
-//         }
-//     }
-//     for (let i = 0; i < fullOperationArray.length; i++) {
-//         if (fullOperationArray.includes("÷")) {
-//             const divideIndex = fullOperationArray.indexOf("÷")
-//             if (fullOperationArray[divideIndex + 1] === 0) {
-//                 fullOperationArray = []
-//                 break
-//             }
-//             const result = fullOperationArray[divideIndex - 1] / fullOperationArray[divideIndex + 1]
-//             fullOperationArray.splice((divideIndex - 1), 3, result)
-//         }
-//     }
-//     for (let i = 0; i < fullOperationArray.length; i++) {
-//         if (fullOperationArray.includes("+")) {
-//             const addIndex = fullOperationArray.indexOf("+")
-//             const result = fullOperationArray[addIndex - 1] + fullOperationArray[addIndex + 1]
-//             fullOperationArray.splice((addIndex - 1), 3, result)
-//         }
-//     }
-//     for (let i = 0; i < fullOperationArray.length; i++) {
-//         if (fullOperationArray.includes("-")) {
-//             const minusIndex = fullOperationArray.indexOf("-")
-//             const result = fullOperationArray[minusIndex - 1] - fullOperationArray[minusIndex + 1]
-//             fullOperationArray.splice((minusIndex - 1), 3, result)
-//         }
-//     }
-
-//     return fullOperationArray[0]
-// }
 
 const compute = () => {
     if (fullOperation.prev === "" || fullOperation.current === "" || fullOperation.operation === "") {
@@ -202,20 +106,15 @@ const compute = () => {
     return computation
 }
 
-
-
 const evaluate = () => {
-
     if (fullOperation.current === 0 && fullOperation.operation === "÷") {
         clear()
         inputElement.innerText = "Error"
     }
-
     result = compute()
     fullOperation.prev = result
     fullOperation.operation = ""
     fullOperation.current = ""
-    console.log(fullOperation)
     return
 }
 
@@ -278,14 +177,12 @@ document.addEventListener('keydown', (event) => {
 
     //single digit number keys
     if (/^\d$/.test(key)) {
-        console.log(key);
         appendNumber(key)
         updateDisplay()
     }
 
     //operators + - * /
     if (/[+\-\*/]/.test(key)) {
-        console.log(key);
         if (key === "*") {
             chooseOperation("x")
             updateDisplay()
